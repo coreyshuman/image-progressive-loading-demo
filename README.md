332459ff065c85a0abdf90d4f8452e6050ea55cd
Corey Shuman <ctshuman89@gmail.com>
2021-12-20 03:50:41 -0800

    feature: add upload and convert capability

---
+++ README.md
@@ -0,0 +1,21 @@
+# Progressive Loading Demo
+
+Experimentation project used to create the linked blog post: 
+
+TODO link
+
+`I demonstrate how to use progressive jpegs and tiny blurred placeholder images to provide a positive low-bandwidth experience when using high quality or large images on a website.`
+
+## How To Use
+
+```bash
+npm install
+
+npm start
+```
+
+Default URL: [localhost:3030](http://localhost:3030)
+
+## Features
+
+Convert any image to a progressive loading webp file of configurable size and quality. This will also output a base64-encoded string which can be used as a preloaded low quality thumbnail, as described in the blog post.
--- routes/index.js
+++ routes/index.js
@@ -2,114 +2,111 @@ var express = require("express");
 var router = express.Router();
 var sharp = require("sharp");
 var path = require("path");
+const fileUpload = require('express-fileupload');
 
 const imageName = "hugues-de-buyer-mimeure-lQPEChtLjUo-unsplash-small.jpg";
 
+router.use(fileUpload({
+  createParentPath: true
+}));
+
 const configs = [{
     type: "png",
-    size: 20,
     formats: [{
       progressive: false,
       adaptiveFiltering: true,
       compressionLevel: 9
-    }, {
-      progressive: true,
-      adaptiveFiltering: true,
-      compressionLevel: 6
-    }, {
-      progressive: false,
-      adaptiveFiltering: true,
-      compressionLevel: 2
     }],
   },
   {
     type: "jpeg",
-    size: 20,
     formats: [{
-        quality: 60,
-        chromaSubsampling: "4:2:0",
-      },
-      {
-        quality: 20,
-        chromaSubsampling: "4:2:0",
-      },
-      {
-        quality: 30,
-        chromaSubsampling: "4:4:4",
-      },
-    ],
+      quality: 50,
+      chromaSubsampling: "4:2:0",
+    }],
   },
   {
     type: "webp",
-    size: 20,
     formats: [{
-        quality: 50
-      },
-      {
-        quality: 50,
-        smartSubsample: true
-      },
-      {
-        quality: 40,
-        smartSubsample: true
-      },
-    ],
+      quality: 50,
+      smartSubsample: true
+    }],
   },
 ];
 
-/* GET home page. */
 router.get("/", async function (req, res, next) {
-  var imagePath = path.join(__dirname, "../public/images/") + imageName;
-  var fs = require("fs");
-  var {
-    size
-  } = fs.statSync(imagePath);
-  size = size / 1024;
-  var image = await sharp(imagePath);
+  res.render("index", {title: "Image Resizer"});
+});
 
-  var medImage = await image
-    .resize(400)
-    .toFormat("jpeg", {
-      quality: 70
-    })
-    .toBuffer();
 
-  var configIdx = 0;
-  var formatIdx = 0;
+router.post("/", async function (req, res, next) {
+  try {
+    const originalImage = req.files?.imageupload;
 
-  for (configIdx = 0; configIdx < configs.length; configIdx++) {
-    var currentConfig = configs[configIdx];
-    
-    for (formatIdx = 0; formatIdx < currentConfig.formats.length; formatIdx++) {
-      var currentFormat = currentConfig.formats[formatIdx];
-      var outputImage = await image
-        .resize(currentConfig.size)
-        .toFormat(currentConfig.type, currentFormat)
-        .toBuffer();
-
-      var outputSize = outputImage.length / 1024;
-      outputImage =
-        `data:image/${currentConfig.type};base64,` +
-        outputImage.toString("base64");
-      
-      if(!currentFormat.string)
-        currentFormat.string = JSON.stringify(currentFormat);
-      currentFormat.outputImage = outputImage;
-      currentFormat.outputSize = outputSize;
+    if(!originalImage) {
+      throw new Error("No image selected.");
     }
+
+    const size = originalImage.size / 1024;
+    var image = await sharp(originalImage.data);
+
+    const medImage = await image
+      .resize(400)
+      .toFormat("jpeg", {
+        quality: 70
+      })
+      .toBuffer();
+
+    var configIdx = 0;
+    var formatIdx = 0;
+
+    for (configIdx = 0; configIdx < configs.length; configIdx++) {
+      var currentConfig = configs[configIdx];
+      
+      for (formatIdx = 0; formatIdx < currentConfig.formats.length; formatIdx++) {
+        var currentFormat = currentConfig.formats[formatIdx];
+        if(currentConfig.type === 'png') {
+          currentFormat.compressionLevel = (100 - req.body.quality) / 10;
+        } else {
+          currentFormat.quality = Number(req.body.quality);
+        }
+        var outputImage = await image
+          .resize(Number(req.body.width))
+          .toFormat(currentConfig.type, currentFormat)
+          .toBuffer();
+
+        var outputSize = outputImage.length / 1024;
+        outputImage =
+          `data:image/${currentConfig.type};base64,` +
+          outputImage.toString("base64");
+        
+        currentFormat.string = undefined;
+        currentFormat.outputImage = undefined;
+        currentFormat.outputSize = undefined;
+        currentFormat.string = JSON.stringify(currentFormat);
+        currentFormat.outputImage = outputImage;
+        currentFormat.outputSize = outputSize;
+      }
+    }
+
+    var medImageSize = medImage.length / 1024;
+    const medImageString = "data:image/jpeg;base64," + medImage.toString("base64");
+    const fullImageString = "data:image/jpeg;base64," + originalImage.data.toString("base64");
+
+    res.render("converted", {
+      title: "Converted Image",
+      size,
+      fullImage: fullImageString,
+      medImage: medImageString,
+      medImageSize,
+      configs,
+    });
+  } catch(err) {
+    res.render("index", {
+      title: 'Image Resizer',
+      message: 'An error occurred:' + err.message
+    });
   }
-
-  var medImageSize = medImage.length / 1024;
-  medImage = "data:image/jpeg;base64," + medImage.toString("base64");
-
-  res.render("index", {
-    title: "Image Resizer",
-    size,
-    fullImage: `/images/${imageName}`,
-    medImage,
-    medImageSize,
-    configs,
-  });
 });
 
 module.exports = router;
